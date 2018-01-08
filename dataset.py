45ac7a8b432e56051d06c1a76242d4fe8dd27475
Zahir Junejo <zjunejo1993@gmail.com>
2018-01-08 02:15:04 +0000

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -36,13 +36,16 @@ joinedData = userPair.join(postPair)
 # Gives out : (total upvotes of user / total posts of user, reputation, total favoriteCount, total fav. count / total posts, total view count / total posts)
 finalData = joinedData.map(lambda x : (x[1][0][0] / x[1][1][0], x[1][0][1], x[1][1][1], x[1][1][1] / x[1][1][0], x[1][1][2] / x[1][1][0]))
 
-clusters = KMeans.train(finalData, 3, maxIterations=100, initializationMode="random")
+clusters = KMeans.train(finalData, k, maxIterations=100, initializationMode="random")
 
 def error(point):
     center = clusters.centers[clusters.predict(point)]
     return sqrt(sum([x**2 for x in (point - center)]))
 
-WSSSE = finalData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
+
+for k in range(3, 15):
+    clusters = KMeans.train(finalData, k, maxIterations=100, initializationMode="random")
+    WSSSE = finalData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
 
 Logger = sc._jvm.org.apache.log4j.Logger
 myLogger = Logger.getLogger(__name__)
