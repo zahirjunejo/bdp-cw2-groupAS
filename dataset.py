9f9ae1679ff1afc58f0a6fa0f5b1ecbdb5a7e2f7
Zahir Junejo <zjunejo1993@gmail.com>
2018-01-08 01:57:35 +0000

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -38,5 +38,11 @@ finalData = joinedData.map(lambda x : (x[1][0][0] / x[1][1][0], x[1][0][1], x[1]
 
 clusters = KMeans.train(finalData, 3, maxIterations=100, initializationMode="random")
 
+def error(point):
+    center = clusters.centers[clusters.predict(point)]
+    return sqrt(sum([x**2 for x in (point - center)]))
+
+WSSSE = finalData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
+
 Logger = sc._jvm.org.apache.log4j.Logger
 myLogger = Logger.getLogger(__name__)
